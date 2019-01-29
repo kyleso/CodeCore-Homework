6cb29f1491e756411f76acbe913d739071522a02
Kyle So <kyle.p.so@gmail.com>
2019-01-28 19:44:58 -0800

    improve turtle_graphics.js, forward() and right/left() methods working

--- turtle_graphics/turtleGraphics.js
+++ turtle_graphics/turtleGraphics.js
@@ -1,67 +1,90 @@
 class Turtle {
   constructor(x, y) {
-    this.x = x,
-    this.y = y
-    this.start = [x, y];
-    this.moveHistory = [this.start];
+    this.x = x;
+    this.y = y;
+    this.moveHistory = [[x, y]];   
     this.direction = 0;
   }
   forward(steps) {
-    
-    let startLocation = [this.x, this.y];
-    let currentLocation = [this.x, this.y];
-    
-    switch(direction) {
-      case 0: // East
-        for (let i = currentLocation[0]; i < steps; i++) {
-          this.moveHistory.push()
-        }
-        this.moveHistory.push([x, y]);
+
+    let startLocation = this.moveHistory[this.moveHistory.length - 1];
+    let direction = this.direction;
+
+    switch (direction) {
+      case 0: // East --> +movement along x-axis  
+        this.moveHistory.push([startLocation[0] + steps, startLocation[1]]);
+        console.log(this.moveHistory);
         break;
-      case 1: // South
-        y += steps;
-        this.moveHistory.push([x, y]);
+      case 1: // South --> +movement along y-axis
+        this.moveHistory.push([startLocation[0], startLocation[1] + steps]);
+        console.log(this.moveHistory);
         break;
-      case 2: // West
-        x -= steps;
-        this.moveHistory.push([x, y]);
+      case 2: // West --> -movement along x-axis
+        this.moveHistory.push([startLocation[0] - steps, startLocation[1]]);
+        console.log(this.moveHistory)
         break;
-      case 3: // North
-        y -= steps;
-        this.moveHistory.push([x, y]);
+      case 3: // North --> -movement along y-axis
+        this.moveHistory.push([startLocation[0], startLocation[1] - steps]);
+        console.log(this.moveHistory)
         break;
       default:
         // do nothing
         break;
     }
+    return this;
   }
   right() {
     if (this.direction === 3) {
       this.direction = 0;
+    } else {
+      this.direction += 1;
     }
-    this.direction += 1;
+    return this;
   }
   left() {
     if (this.direction === 0) {
       this.direction = 3;
+    } else {
+      this.direction -= 1;
     }
-    this.direction -= 1;
+    return this;
   }
-  allPoints() {
-    return this.moveHistory;
-  }
-  print(points) {
-    
-    let points = this.allPoints;
-    let xAxisArray = [];
-    let yAxisArray = [];
-
-    for (let i = 0; i < points.length; i++) {
-      xAxisArray.push(points[i][0]);
-      yAxisArray.push(points[i][1]);
-    }
-    let maxGridWidth = Math.max(...xAxisArray) + 1;
-    let maxGridHeight = Math.max(...yAxisArray) + 1;
-  }
+  // allPoints() {
+    // let points = [];
+    // for ()
 }
+
+
+const flash = new Turtle(0, 0)
+.forward(3)
+.right().forward(4).right().forward(2)
+// .forward(3);
+console.log(flash)
+
+
+
+let coord1 = [3, 4];
+let coord2 = [7, 4];
+let coord3 = [3, 1];
+
+let startX = coord1[0];
+let endX = coord2[0];
+let valueY = coord1[1];
+
+console.log(startX);
+console.log(endX)
+console.log(valueY)
+
+const diffBetweenPoints = endX - startX + 1;
+console.log(diffBetweenPoints)
+let arrayX = Array.from(Array(diffBetweenPoints), (x, index) => startX + index);
+console.log(arrayX);
+
+let points = [];
+
+for (let i = 0; i < arrayX.length; i++) {
+  points.push([arrayX[i], valueY]);
+}
+
+console.log(points)
 
