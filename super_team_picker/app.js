830673353475d7c3d94a4ee0049757c9d7b06fb3
Kyle So <kyle.p.so@gmail.com>
2019-02-10 18:09:17 -0800

    add routes for new, show, index with views

--- super_team_picker/app.js
+++ super_team_picker/app.js
@@ -1,22 +1,39 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const logger = require('morgan');
-
+const cookieParser = require('cookie-parser');
+const methodOverride = require('method-override')
 
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+	methodOverride((req, res) => {
+		if (req.body && req.body._method) {
+			const method = req.body._method;
+			return method;
+		}
+	}),
+);
+app.use((request, response, next) => {
+  const username = request.cookies.username;
+  response.locals.username = username || '';
+  next();
+});
 
 
 app.get('/', (req, res) => {
-  res.send('home')
   res.render('home')
 })
 
 const cohortsRouter = require('./routes/cohortsRouter');
 app.use('/cohorts', cohortsRouter);
 
-const PORT = 4001;
+const PORT = 5656;
 const HOST = 'localhost';
 app.listen(PORT, HOST, () => {
   console.log(`The server is listening at http://${HOST}:${PORT}`)
--- super_team_picker/routes/cohortsRouter.js
+++ super_team_picker/routes/cohortsRouter.js
@@ -1,8 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const knex = require("../db/client");
 
-router.get('/', (req, res) => {
-  res.send('GET cohorts is working');
+router.get("/", (req, res) => {
+  knex("cohorts")
+    .orderBy("createAt", "desc")
+    .then(cohorts => {
+      res.render("cohorts/index", { cohorts: cohorts });
+    });
+});
+
+router.get("/new", (req, res) => {
+  res.render("cohorts/new");
+});
+
+router.post("/", (req, res) => {
+  const newCohort = req.body;
+  knex("cohorts")
+    .insert(newCohort)
+    .returning("*")
+    .then(cohorts => {
+			const [cohort] = cohorts;
+			res.redirect(`/cohorts/${cohort.id}`);
+		});
+});
+
+router.get('/:id', (req, res) => {
+	const id = req.params.id;
+	knex('cohorts')
+		.where('id', id)
+		.first()
+		.then(cohort => {
+			res.render('cohorts/show', { cohort: cohort });
+		});
 });
 
 module.exports = router;
