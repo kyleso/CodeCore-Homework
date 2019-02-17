f9542e2f875c835e7163f152250d7e6e28e477d4
Kyle So <kyle.p.so@gmail.com>
2019-02-17 14:42:21 -0800

    create post model, installed and set up requires and imports for bootstrap

--- blog_on_rails/db/seeds.rb
+++ blog_on_rails/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.destroy_all
+
+50.times do
+
+  created_at = Faker::Date.backward(365 * 2)
+  Post.create(
+    title: Faker::Hacker.say_something_smart,
+    body: Faker::ChuckNorris.fact,
+    created_at: created_at,
+    updated_at: created_at
+  )
+end
+
+
