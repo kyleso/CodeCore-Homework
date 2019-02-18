86202f7fbf594fee7a43996aa7005e4202e08e46
Kyle So <kyle.p.so@gmail.com>
2019-02-17 18:47:53 -0800

    add nav bar

--- blog_on_rails/app/models/post.rb
+++ blog_on_rails/app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  posts = Post.all
+  puts posts
 
   validates(:title, presence: true, uniqueness: true)
   validates(:body, presence: true, length: { minimum: 50 })
