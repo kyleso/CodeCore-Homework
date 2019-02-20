54e5fb10257ea18629277f5ca6d4753974c3cc20
Kyle So <kyle.p.so@gmail.com>
2019-02-19 19:33:58 -0800

    add routes, actions, and views for show, edit, delete.  Fix styling for pages with some bootstrap

--- blog_on_rails/app/controllers/posts_controller.rb
+++ blog_on_rails/app/controllers/posts_controller.rb
@@ -1,15 +1,40 @@
 class PostsController < ApplicationController
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+    if @post.save
+      redirect_to post_path(@post.id)
+    else
+      render :new
+    end
+  end
+
   def index 
-    @posts = Post.all 
+    @posts = Post.all.order(created_at: :desc) 
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
-  def new
-    @title = params[:title]
-    @body = params[:body]
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy 
+    redirect_to posts_path
+  end
+
+  private_methods
+  
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 
 end
--- blog_on_rails/config/routes.rb
+++ blog_on_rails/config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   get("/", to: "posts#index", as: :root)
   get("/posts", to: "posts#index")
 
-  get("/posts/new", to: "posts#new")
+  get("/posts/new", to: "posts#new", as: :new_post)
+  post("/posts", to: "posts#create")
 
-  get("/posts/:id", to: "posts#show")
-  # get("/posts/:id/edit" to: "posts#edit")
-  # patch("/:id")
+  get("/posts/:id", to: "posts#show", as: :post)
+  get("/posts/:id/edit", to: "posts#edit", as: :edit_post)
+  patch("/posts/:id", to: "posts#update")
+  delete("/posts/:id", to: "posts#destroy")
 
 end
