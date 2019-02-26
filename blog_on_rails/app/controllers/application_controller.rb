037448d95c6983504ab9ef7d490aa0ec924fb230
Kyle So <kyle.p.so@gmail.com>
2019-02-26 00:39:25 -0800

    add sessions controller, update application.html.erb layout, add new views for new_user, new_session, update new_post

--- blog_on_rails/app/controllers/application_controller.rb
+++ blog_on_rails/app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def current_user
+    if session[:user_id].present?
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+  helper_method(:current_user)
+
+  def user_signed_in?
+    current_user.present?
+  end
+  helper_method(:user_signed_in?)
+
+  def authenticated_user!
+    unless user_signed_in?
+      flash[:alert] = "You Must Sign In or Sign Up First"
+      redirect_to root_path
+    end
+  end
 end
