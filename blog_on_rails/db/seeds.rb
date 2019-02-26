90b8b7dc654296ca97b228c476871af8e42e84b9
Kyle So <kyle.p.so@gmail.com>
2019-02-25 18:23:49 -0800

    add user model, migration, and updated seed file

--- blog_on_rails/db/seeds.rb
+++ blog_on_rails/db/seeds.rb
@@ -5,23 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+PASSWORD = "123"
+
 Comment.destroy_all
 Post.destroy_all
+User.destroy_all
 
 50.times do
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  User.create(
+    name: name,
+    email: email,
+    password: PASSWORD
+  )
+end
 
+users = User.all
+
+50.times do
   created_at = Faker::Date.backward(365 * 2)
 
   p = Post.create(
     title: Faker::Book.title,
     body: Faker::ChuckNorris.fact,
     created_at: created_at,
-    updated_at: created_at
+    updated_at: created_at,
+    user: users.sample
   )
 
   if p.valid?
     p.comments = rand(0..25).times.map do
-      Comment.new(body: Faker::GreekPhilosophers.quote)
+      Comment.new(
+        body: Faker::GreekPhilosophers.quote
+        created_at: created_at,
+        updated_at: created_at,
+        user: users.sample
+        )
     end
   end
 end
