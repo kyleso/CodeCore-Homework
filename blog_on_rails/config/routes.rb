09cec49b93369d3d24e21efc37107758c40243f5
Kyle So <kyle.p.so@gmail.com>
2019-02-27 12:15:18 -0800

    clean up routes by removing unnecessary comments

--- blog_on_rails/config/routes.rb
+++ blog_on_rails/config/routes.rb
@@ -17,14 +17,4 @@ Rails.application.routes.draw do
   end
 
   get("/", to: "posts#index", as: :root)
-  # get("/posts", to: "posts#index")
-
-  # get("/posts/new", to: "posts#new", as: :new_post)
-  # post("/posts", to: "posts#create")
-
-  # get("/posts/:id", to: "posts#show", as: :post)
-  # get("/posts/:id/edit", to: "posts#edit", as: :edit_post)
-  # patch("/posts/:id", to: "posts#update")
-  # delete("/posts/:id", to: "posts#destroy")
-
 end
