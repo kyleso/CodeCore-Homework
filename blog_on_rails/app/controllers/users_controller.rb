4ebfab549e09923d4697dd2cd825861fb860ca3e
Kyle So <kyle.p.so@gmail.com>
2019-02-26 19:03:50 -0800

    fix bugs where user could edit or input an empty name, and also redirection error in users controller

--- blog_on_rails/app/controllers/users_controller.rb
+++ blog_on_rails/app/controllers/users_controller.rb
@@ -9,9 +9,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       session[:user_id] = @user.id
-      if session[:return_to].present?
-        redirect_to session.delete(:return_to), notice: "Logged In Successfully"
-      end
+      redirect_to root_path
     else
       render :new
     end
--- blog_on_rails/app/models/user.rb
+++ blog_on_rails/app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  validates(:name, presence: true)
   validates(:email, presence: true,
                     uniqueness: true,
                     format: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
-
 end
