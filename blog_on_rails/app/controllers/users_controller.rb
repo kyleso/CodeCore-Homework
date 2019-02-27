ba46c6733c8589085278d172e788c983ddaba6f3
Kyle So <kyle.p.so@gmail.com>
2019-02-26 17:38:20 -0800

    add functionality for users to edit their name and email, with link in navbar

--- blog_on_rails/app/controllers/users_controller.rb
+++ blog_on_rails/app/controllers/users_controller.rb
@@ -1,17 +1,32 @@
 class UsersController < ApplicationController
+  before_action :authenticated_user!, only: [:edit, :update]
+
   def new
     @user = User.new
   end
 
-  def create 
+  def create
     @user = User.new user_params
     if @user.save
       session[:user_id] = @user.id
       if session[:return_to].present?
         redirect_to session.delete(:return_to), notice: "Logged In Successfully"
-      end 
+      end
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update user_params
+      redirect_to root_path
+    else
+      render :edit
     end
   end
 
