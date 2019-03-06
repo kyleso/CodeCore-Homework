7d2b1b1a09ea87a12be6dc7f9d4eff13ce3db2b0
Kyle So <kyle.p.so@gmail.com>
2019-03-06 12:57:09 -0800

    add guessed letters end up on the proper line in the blanks

---
+++ hangman/main.js
@@ -0,0 +1,91 @@
+const animals = [
+  "alligator",
+  "ant",
+  "bear",
+  "bee",
+  "bird",
+  "camel",
+  "cat",
+  "cheetah",
+  "chicken",
+  "chimpanzee",
+  "cow",
+  "crocodile",
+  "deer",
+  "dog",
+  "dolphin",
+  "duck",
+  "eagle",
+  "elephant",
+  "fish",
+  "fly",
+  "fox",
+  "frog",
+  "giraffe",
+  "goat",
+  "goldfish",
+  "hamster",
+  "hippopotamus",
+  "horse",
+  "kangaroo",
+  "kitten",
+  "lion",
+  "lobster",
+  "monkey",
+  "octopus",
+  "owl",
+  "panda",
+  "pig",
+  "puppy",
+  "rabbit",
+  "rat",
+  "scorpion",
+  "seal",
+  "shark",
+  "sheep",
+  "snail",
+  "snake",
+  "spider",
+  "squirrel",
+  "tiger",
+  "turtle",
+  "wolf",
+  "zebra"
+];
+
+const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
+const animalLetters = randomAnimal.split("");
+
+const guessedLetters = [];
+
+$(document).ready(() => {
+  console.log(animalLetters);
+
+  for (let i = 0; i < animalLetters.length; i++) {
+    $(".guess-area").append(
+      $('<div class="guess-line" style="border-bottom: solid black 2px"></div>')
+    );
+  }
+
+  
+  $(".alpha-btn").on("click", event => {
+    $(event.currentTarget).addClass("clicked");
+    $(event.currentTarget).attr("disabled", true);
+    // guessedLetters.push($(event.currentTarget).text())
+
+    const indicesOfLetter = [];
+    for (let i = 0; i < animalLetters.length; i++) {
+      if (
+        animalLetters[i] ===
+        $(event.currentTarget)
+          .text()
+          .toLowerCase()
+      ) {
+        indicesOfLetter.push(i);
+        $(".guess-line").eq(i)[0].innerText = `${animalLetters[i].toUpperCase()}`;
+      }
+    }
+  });
+
+
+});
