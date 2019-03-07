5cdbfb62a2deecd730e810550c491a36278a9939
Kyle So <kyle.p.so@gmail.com>
2019-03-06 17:14:15 -0800

    clean up styling for guess-area

--- hangman/main.js
+++ hangman/main.js
@@ -67,25 +67,23 @@ $(document).ready(() => {
     );
   }
 
-  
+  let correctCount = 0;
+  // while (correctCount < animalLetters.length) {
+    
+  // }
   $(".alpha-btn").on("click", event => {
     $(event.currentTarget).addClass("clicked");
     $(event.currentTarget).attr("disabled", true);
     // guessedLetters.push($(event.currentTarget).text())
 
     const indicesOfLetter = [];
+    
     for (let i = 0; i < animalLetters.length; i++) {
-      if (
-        animalLetters[i] ===
-        $(event.currentTarget)
-          .text()
-          .toLowerCase()
-      ) {
+      if (animalLetters[i] === $(event.currentTarget).text().toLowerCase()) {
         indicesOfLetter.push(i);
-        $(".guess-line").eq(i)[0].innerText = `${animalLetters[i].toUpperCase()}`;
-      }
-    }
+        $(".guess-line").eq(i)[0].innerText = `${animalLetters[i].toUpperCase()}`;   
+        correctCount += 1; 
+      }    
+    };
   });
-
-
 });
